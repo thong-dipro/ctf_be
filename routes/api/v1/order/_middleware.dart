f0ad69ec769c3d914b95e82f00f004a2a97dcb5c
thong-dipro <thongtd@dipro.vn>
2023-11-13 22:27:48 +0700

    feat: add middleware cors

--- routes/api/v1/order/_middleware.dart
+++ routes/api/v1/order/_middleware.dart
@@ -4,9 +4,24 @@ import 'package:backend/api/v1/profile/repository/profile_repository.dart';
 import 'package:backend/api/v1/profile/service/profile_service.dart';
 import 'package:backend/core/database/database.dart';
 import 'package:dart_frog/dart_frog.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart' as shelf;
 
 Handler middleware(Handler handler) {
   return handler
+      .use(
+        fromShelfMiddleware(
+          shelf.corsHeaders(
+            headers: {
+              shelf.ACCESS_CONTROL_ALLOW_ORIGIN:
+                  '*', // Required for CORS support to work
+              shelf.ACCESS_CONTROL_ALLOW_METHODS:
+                  'GET, POST, OPTIONS, PUT, DELETE, HEAD',
+              shelf.ACCESS_CONTROL_ALLOW_HEADERS:
+                  'Origin, Content-Type, X-Auth-Token',
+            },
+          ),
+        ),
+      )
       .use(
         profileServiceProvider(),
       )
